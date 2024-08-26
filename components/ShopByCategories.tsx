363c72e5167408b6b6b2bca86a027b65baba098e
HajiAli <senayousofiali@gmail.com>
2024-08-26 16:18:28 +0430

    added: collections page and single carpet page

--- components/ShopByCategories.tsx
+++ components/ShopByCategories.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { carpetCategories } from '@/carpet categories/carpetCategories'
 import Categories from '@/util components/Categories'
 
-export default function ShopByCategories() {
+export default function ShopByCategories({text} : {text: boolean}) {
+    
     return (
         <div>
-            <h1 className="text-3xl sm:text-4xl  lg:text-5xl font-bold text-center mb-8  mt-32">
-                Shop By <span className="text-orange-500"> Categories </span>
-            </h1>
+
+            {
+                text
+                ?
+                <h1 className="text-3xl sm:text-4xl  lg:text-5xl font-bold text-center mb-8  mt-32">
+                    Shop By <span className="text-orange-500"> Categories </span>
+                </h1>
+                :
+                <h1 className="text-3xl sm:text-4xl  lg:text-5xl font-bold text-center mb-8  mt-32">
+                    You may also <span className="text-orange-500"> Like </span>
+                </h1>
+                
+
+            }
 
             <div className="flex mx-auto max-w-7xl gap-x-10 gap-y-10 justify-center items-center flex-wrap">
                 {
